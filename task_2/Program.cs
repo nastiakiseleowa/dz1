6bd87a8a0795a4174b22f51e3edfa390fbb460ab
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-09 13:13:56 +0300

    add task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write ("Введите число a ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("Введите число b ");
+int numberb = Convert.ToInt32(Console.ReadLine());
+ if (numbera > numberb)
+    {
+        Console.WriteLine(numbera);
+    }
+    else if (numberb > numbera)
+    {
+        Console.WriteLine(numberb);
+    }
