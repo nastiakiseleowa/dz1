53e4fd9ecf87c7070fc95db92cc7cce429882bda
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-09 15:56:33 +0300

    add task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿while(true){
+Console.Write("Введите число a ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b ");
+int numberb = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число c ");
+int numberc = Convert.ToInt32(Console.ReadLine());
+
+if (numbera > numberb)
+{
+    if (numbera > numberc)
+        Console.WriteLine(numbera);
+    else
+        Console.WriteLine(numberc);
+}
+else if (numberb > numberc)
+    Console.WriteLine(numberb);
+else
+    Console.WriteLine(numberc);
+}
