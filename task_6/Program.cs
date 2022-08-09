d597473bcf2676785cf6e161a81418c76a4b2578
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-09 15:57:26 +0300

    add task 6

---
+++ task_6/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число a ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+
+if (Convert.ToBoolean(numbera % 2)) // число нечетное
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("да"); // число четное
+}
+
+
+/*
+Console.Write ("Введите число a ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+int a = numbera / 2;
+int b = (numbera + 1) / 2;
+
+if (a==b)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
+*/
