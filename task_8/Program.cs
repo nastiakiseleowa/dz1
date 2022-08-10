4143c3580fd0c710798a78ebff0528e217b7af8b
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-10 12:16:17 +0300

    add task 8

---
+++ task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+Console.Write("Четные числа от 1 до N: ");
+
+while (i < n)
+{
+    Console.Write(i);
+    i = i + 2;
+    if (i < n)
+        Console.Write(", ");
+    else
+        Console.Write(".");
+}
